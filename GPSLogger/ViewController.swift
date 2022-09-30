076991a36c1a1fcfe9ca40298b8e011b0784eb41
curoky <cccuroky@gmail.com>
2022-09-30 22:38:56 +0800

    build(xcode): init Xcode project

---
+++ GPSLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPSLogger
+//
+//  Created by cicada on 2022/9/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
