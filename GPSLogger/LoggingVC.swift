be116945277c7d489ebbb433c9d3d1b49d3ea7c3
curoky <cccuroky@gmail.com>
2022-09-30 22:48:49 +0800

    feat(view): add tab view with three tabs

---
+++ GPSLogger/LoggingVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoggingVC.swift
+//  GPSLogger
+//
+//  Created by curoky on 2022/9/30.
+//
+
+import UIKit
+
+class LoggingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GPSLogger/StateVC.swift
+++ GPSLogger/StateVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StateVC.swift
 //  GPSLogger
 //
 //  Created by curoky on 2022/9/30.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StateVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
