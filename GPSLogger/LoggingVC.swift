cebd3030bc2dbdaacdeadfab8c72664ca823639f
curoky <cccuroky@gmail.com>
2022-10-01 23:23:06 +0800

    feat(view): add log viewer and export button

--- GPSLogger/LoggingVC.swift
+++ GPSLogger/LoggingVC.swift
@@ -8,14 +8,28 @@
 import UIKit
 
 class LoggingVC: UIViewController {
-
+    
+    @IBOutlet weak var logTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func fetchLogClicked(_ sender: Any) {
+        logTextView.text = GPSLogHelper.shared.tailfLog()
+    }
+    
+    @IBAction func exportClicked(_ sender: Any) {
+        var filesToShare = [Any]()
+        filesToShare.append(URL(filePath: GPSLogHelper.shared.logFilePath))
+        
+        // Make the activityViewContoller which shows the share-view
+        let activityViewController = UIActivityViewController(activityItems: filesToShare, applicationActivities: nil)
+        // Show the share-view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
