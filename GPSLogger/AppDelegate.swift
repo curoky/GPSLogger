e9cf88493a6013e4f15b2137b0e4de94c96941b3
curoky <cccuroky@gmail.com>
2022-10-01 23:22:15 +0800

    feat(main): enable backgroud location tracer

--- GPSLogger/AppDelegate.swift
+++ GPSLogger/AppDelegate.swift
@@ -11,10 +11,11 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var backgroudLocationTracer = BackgroudLocationTracer()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        backgroudLocationTracer.startMonitoring()
         return true
     }
 
