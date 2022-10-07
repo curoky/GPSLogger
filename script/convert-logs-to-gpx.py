60c6fa96e6af244c6a151642c7f9806b242c713d
curoky <cccuroky@gmail.com>
2022-10-07 18:12:52 +0800

    style(license): update file header license

--- script/convert-logs-to-gpx.py
+++ script/convert-logs-to-gpx.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-#
 # Copyright (c) 2022-2022 curoky(cccuroky@gmail.com).
 #
 # This file is part of GPSLogger.
-# See https://github.com/curoky/GPSLogger for further info.
+# See https://github.com/curoky/GPSLogger.git for further info.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 import gpxpy.gpx
 from pathlib import Path
