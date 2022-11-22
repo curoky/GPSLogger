01c7deb4f63a106bb8ff213444a6cea317a9ac7f
curoky <cccuroky@gmail.com>
2022-11-23 02:13:39 +0800

    feat(UI): complete basic ui

--- GPSLogger/ContentView.swift
+++ GPSLogger/ContentView.swift
@@ -17,82 +17,38 @@
  * limitations under the License.
  */
 
-import CoreData
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default
-    )
-    private var items: FetchedResults<Item>
+    @State private var onHighPrecision = false
+    @State private var logMessage = ""
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        VStack {
+            TextEditor(text: .constant(logMessage))
+                .foregroundColor(.black)
+                .padding(.horizontal)
+                .font(/*@START_MENU_TOKEN@*/ .title3/*@END_MENU_TOKEN@*/)
+
+            HStack {
+                Toggle(isOn: $onHighPrecision) {}.labelsHidden()
+
+                Button(action: {}) {
+                    Image(systemName: "square.and.arrow.up")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .buttonStyle(.borderedProminent)
+
+                Button(action: {
+                    logMessage += "123\n"
+                }) {
+                    Image(systemName: "gobackward")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
+                .buttonStyle(.borderedProminent)
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            }.padding()
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
