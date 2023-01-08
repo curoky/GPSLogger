c84ec1a06529abbe48505ddf71ca2ff02c5fe5e5
curoky <cccuroky@gmail.com>
2023-01-09 00:17:20 +0800

    style(pre-commit): format code

--- script/convert-logs-to-gpx.py
+++ script/convert-logs-to-gpx.py
@@ -16,8 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import gpxpy.gpx
 from pathlib import Path
+
+import gpxpy.gpx
 from dateutil.parser import parse
 
 
