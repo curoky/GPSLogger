15a6e310da6707296ac614792d8dbb01a1da58ac
curoky <cccuroky@gmail.com>
2023-09-16 14:23:51 +0800

    feat(gps): merge logger to tracer

--- GPSLogger/BackgroudLocationTracer.swift
+++ GPSLogger/BackgroudLocationTracer.swift
@@ -20,17 +20,20 @@
 import CoreData
 import CoreLocation
 import Foundation
+import SwiftUI
 
-typealias Listener = (CLLocation) -> Void
-
-class BackgroudLocationTracer: NSObject {
-    static let shared = BackgroudLocationTracer()
-    var isOnHighPrecision = false
+class BackgroudLocationTracer: NSObject, ObservableObject {
     var locationManager = CLLocationManager()
-    var lastSwithedHighPrecisionTime = Date.now
+    var stopedLocation: [CLLocation] = []
+
+    @Published var currentLocation: CLLocationCoordinate2D = kCLLocationCoordinate2DInvalid
+    @Published var currentAltitude: Double = 0
+    @Published var updatedCount: Int = 0
+    @Published var isOnHighPrecision: Bool = false
+    @Published var lastSwithedHighPrecisionTime = Date.now
+    @Published var currentMessage: String = ""
 
     func startMonitoring() {
-        GPSLogHelper.shared.log(message: "====== startMonitoring \n")
         locationManager.requestAlwaysAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.pausesLocationUpdatesAutomatically = false
@@ -45,60 +48,81 @@ class BackgroudLocationTracer: NSObject {
         if isOnHighPrecision == highPrecision {
             return
         }
-        GPSLogHelper.shared.log(message: "====== switchTracerMode \(highPrecision)\n")
         isOnHighPrecision = highPrecision
         if highPrecision {
             lastSwithedHighPrecisionTime = Date.now
-            GPSLogHelper.shared.log(message: "====== startUpdatingLocation \n")
             locationManager.stopMonitoringSignificantLocationChanges()
             locationManager.startUpdatingLocation()
         } else {
-            GPSLogHelper.shared.log(message: "====== startMonitoringSignificantLocationChanges \n")
             locationManager.stopUpdatingLocation()
             locationManager.startMonitoringSignificantLocationChanges()
         }
+    }
+
+    func getLogFileURL() -> URL? {
+        if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            return documentsDirectory.appendingPathComponent("gps_log.txt")
+        }
+        return nil
     }
 }
 
 extension BackgroudLocationTracer: CLLocationManagerDelegate {
     func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         for loc in locations {
-            let encoder = JSONEncoder()
-            encoder.outputFormatting = .sortedKeys
-            let jsonData = try! encoder.encode(["timestamp": loc.timestamp.description,
-                                                "speed": loc.speed.description,
-                                                "latitude": loc.coordinate.latitude.description,
-                                                "longitude": loc.coordinate.longitude.description,
-                                                "altitude": loc.altitude.description,
-                                                "course": loc.course.description,
-                                                "speedAccuracy": loc.speedAccuracy.description,
-                                                "ellipsoidalAltitude": loc.ellipsoidalAltitude.description,
-                                                "horizontalAccuracy": loc.horizontalAccuracy.description,
-                                                "verticalAccuracy": loc.verticalAccuracy.description,
-                                                "courseAccuracy": loc.courseAccuracy.description])
-            GPSLogHelper.shared.log(message: (String(data: jsonData, encoding: .utf8) ?? "error in loc to json") + "\n")
+            currentAltitude = loc.altitude
+            currentLocation = loc.coordinate
+            updatedCount += 1
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: ["timestamp": loc.timestamp.description,
+                                                                           "speed": loc.speed.description,
+                                                                           "latitude": loc.coordinate.latitude.description,
+                                                                           "longitude": loc.coordinate.longitude.description,
+                                                                           "altitude": loc.altitude.description,
+                                                                           "course": loc.course.description,
+                                                                           "speedAccuracy": loc.speedAccuracy.description,
+                                                                           "ellipsoidalAltitude": loc.ellipsoidalAltitude.description,
+                                                                           "horizontalAccuracy": loc.horizontalAccuracy.description,
+                                                                           "verticalAccuracy": loc.verticalAccuracy.description,
+                                                                           "courseAccuracy": loc.courseAccuracy.description], options: .sortedKeys)
+
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    currentMessage = jsonString
+                }
+
+                if let logFileURL = getLogFileURL() {
+                    if !FileManager.default.fileExists(atPath: logFileURL.path()) {
+                        FileManager.default.createFile(atPath: logFileURL.path(), contents: nil)
+                    }
+
+                    let fileHandle = try FileHandle(forWritingTo: logFileURL)
+                    fileHandle.seekToEndOfFile()
+                    fileHandle.write(jsonData)
+                    fileHandle.closeFile()
+                }
+
+            } catch {
+                print("保存JSON文件失败：\(error)")
+            }
 
             if isOnHighPrecision {
-                for mp in MY_LIVE_POSITIONS {
+                for mp in stopedLocation {
                     if loc.distance(from: mp) < 50 {
                         let diffComponents = Calendar.current.dateComponents([.minute], from: lastSwithedHighPrecisionTime, to: Date.now)
                         if diffComponents.minute! < 10 {
-                            GPSLogHelper.shared.log(message: "====== < 50 but not stop for(\(diffComponents.minute!) minute) \n")
                         } else {
-                            GPSLogHelper.shared.log(message: "====== < 50 stop \n")
                             switchTracerMode(highPrecision: false)
                         }
                     }
                 }
             } else {
                 var inAnyOne = false
-                for mp in MY_LIVE_POSITIONS {
+                for mp in stopedLocation {
                     if loc.distance(from: mp) < 100 {
                         inAnyOne = true
                     }
                 }
                 if !inAnyOne {
-                    GPSLogHelper.shared.log(message: "====== > 100 start \n")
                     switchTracerMode(highPrecision: true)
                 }
             }
